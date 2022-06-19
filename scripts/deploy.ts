88f56ba4fc86a5ea7056630254d59e52b1dbb0aa
moneeb <moneebalalfi@gmail.com>
2022-06-19 10:39:53 +0300

    feat: set the hardhat config and deploy the contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,29 +1,24 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
+const fs = require("fs");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  const Blog = await ethers.getContractFactory("Blog");
+  const blog = await Blog.deploy("My web3 blog");
 
-  // We get the contract to deploy
-  const Greeter = await ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  await blog.deployed();
+  console.log("Blog deployed to:", blog.address);
 
-  await greeter.deployed();
+  const ownerAddress = await blog.signer.getAddress();
 
-  console.log("Greeter deployed to:", greeter.address);
+  fs.writeFileSync(
+    "./config.ts",
+    `
+  export const contractAddress = "${blog.address}"
+  export const ownerAddress = "${ownerAddress}"
+  `
+  );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
